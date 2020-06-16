c555b2ead4eb5d3283534ac6afc4d16ded4b76b5
thanh2107 <tyn01685732770@gmail.com>
2020-06-16 09:44:17 +0700

    Thêm màng hình chào

--- app/src/main/java/com/example/docsach/helloActivity.java
+++ app/src/main/java/com/example/docsach/helloActivity.java
@@ -1,6 +1,8 @@
 package com.example.docsach;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class helloActivity extends AppCompatActivity {
@@ -9,5 +11,27 @@ public class helloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        //Dùng cài đặt sau 5 giây màn hình tự chuyển
+        Thread bamgio=new Thread(){
+            public void run()
+            {
+                try {
+                    sleep(2500);
+                } catch (Exception e) {
+
+                }
+                finally
+                {
+                    Intent activitymoi=new Intent("com.example.taomanhinhchao.MAINACTIVITY");
+                    startActivity(activitymoi);
+                }
+            }
+        };
+        bamgio.start();
+    }
+    //sau khi chuyển sang màn hình chính, kết thúc màn hình chào
+    protected void onPause(){
+        super.onPause();
+        finish();
     }
 }
