bbdecfe3dc124e69aef5e16f5e5500092368b69a
thaibao <thaibaonguyen090@gmail.com>
2020-06-21 19:55:29 +0700

    display ds yeu thich

--- app/src/main/java/com/example/docsach/FavoriteFragment.java
+++ app/src/main/java/com/example/docsach/FavoriteFragment.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,6 +23,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -35,8 +43,13 @@ public class FavoriteFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myData = database.getReference();
+    List<String> bookId_list;
+    List<Book> lstRoot;
+    List<Book> lstBook;
+    List<Favorite_item> fv_list;
+    FirebaseUser user;
+    String uID;
+    View view;
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -73,44 +86,50 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_favorite, container, false);
-        Context context = container.getContext();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        view =  inflater.inflate(R.layout.fragment_favorite, container, false);
+        final Context context = container.getContext();
+
+        fv_list = new ArrayList<>();
+        lstBook = new ArrayList<>();
+        myData m = new myData();
+        lstRoot = m.myList();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uID = user.getUid().toString();
+        Log.d("IDDDD1", uID);
         if (user != null) {
-            String id = user.getUid().toString();
-            myData.child("Favorite").addChildEventListener(new ChildEventListener() {
+            DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+            DatabaseReference moviesRef = rootRef.child("Favorite");
+            ValueEventListener eventListener = new ValueEventListener() {
                 @Override
-                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                        Favorite_item item = ds.getValue(Favorite_item.class);
+                        String id = item.uId;
+                        if(id.equals(uID) ){
+                            Log.d("aaa", uID);
+                            String stt = item.bookId;
+                            for(Book b: lstRoot){
+                                if(stt.equals(b.getStt().toString()) ){
+                                    lstBook.add(b);
+                                }
+                            }
+                        }
+                    }
+                    //display
+                    RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(context, lstBook);
+                    RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recycleview_favorite_id);
+                    recyclerView.setLayoutManager(new GridLayoutManager(context, 3));
+                    recyclerView.setAdapter(myAdapter);
 
                 }
 
                 @Override
-                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
-            //Toast.makeText(context, user.getUid().toString(), Toast.LENGTH_LONG).show();
+                public void onCancelled(DatabaseError databaseError) {}
+            };
+            moviesRef.addListenerForSingleValueEvent(eventListener);
         }
-
-
-
         return view;
     }
-
 }
 
--- app/src/main/java/com/example/docsach/Favorite_item.java
+++ app/src/main/java/com/example/docsach/Favorite_item.java
@@ -4,8 +4,25 @@ public class Favorite_item {
     public String uId;
     public String bookId;
 
+    public Favorite_item(){}
     public Favorite_item(String uId, String bookId) {
         this.uId = uId;
         this.bookId = bookId;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookId() {
+        return bookId;
     }
 }
